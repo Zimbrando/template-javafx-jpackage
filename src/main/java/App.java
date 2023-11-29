e754284f852da37a737f0be5d77bce8b4afa0ccc
Marco Sternini <mkarko01@gmail.com>
2023-11-29 11:33:11 +0100

    Initial commit

---
+++ src/main/java/App.java
@@ -0,0 +1,12 @@
+
+import javafx.application.Application;
+
+public final class App {
+
+    private App() {
+    }
+
+    public static void main(final String[] args) {
+        Application.launch(MainStage.class, args);
+    }
+}
---
+++ src/main/java/MainStage.java
@@ -0,0 +1,12 @@
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainStage extends Application {
+
+    @Override
+    public final void start(final Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        primaryStage.show();
+    }
+}
